a4accf48f502ae63ecee8722d49295dbec5ab480
AnkitAgarwal <ankit.agarwal@alaskaair.com>
2016-03-28 21:54:36 -0700

    added a new project for asp.net training

---
+++ TrainingTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace TrainingTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is a message from me";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TrainingTest/Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace TrainingTest.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Browse()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            ViewBag.Message = "This is sent from the controller";
+            return View();
+        }
+    }
+}
